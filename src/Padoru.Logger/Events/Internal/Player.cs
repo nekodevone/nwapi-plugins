cda82827f15b5aa169b43c6b1e9deb4910449bb2
Nekonyx <nekonyx@proton.me>
2022-12-24 04:14:11 +0600

    fix(logger): забытая скобка в конце

--- src/Padoru.Logger/Events/Internal/Player.cs
+++ src/Padoru.Logger/Events/Internal/Player.cs
@@ -110,100 +110,103 @@ namespace Padoru.Logger.Events.Internal
         }
 
         [PluginEvent(ServerEventType.PlayerThrowProjectile)]
-        public void OnPlayerThrowProjectile(PlayerAPI player , ThrowableItem ThrItem)
+        public void OnPlayerThrowProjectile(PlayerAPI player, ThrowableItem ThrItem)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) бросил {ThrItem}.");
         }
-        
+
         [PluginEvent(ServerEventType.Scp914Activate)]
         public void OnScp914Activate(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) использовал SCP-914");
-        } 
-        
+        }
+
         [PluginEvent(ServerEventType.Scp914KnobChange)]
         public void OnScp914KnobChange(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) переключил режим SCP-914");
-        } 
-        
+        }
+
         [PluginEvent(ServerEventType.Scp939CreateAmnesticCloud)]
         public void OnScp939CreateAmnesticCloud(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) выпустил амнеизак за SCP-939");
         }
 
-    
-    [PluginEvent(ServerEventType.Scp079LevelUpTier)]
-        public void OnScp079LevelUpTier(PlayerAPI player , int int079)
+
+        [PluginEvent(ServerEventType.Scp079LevelUpTier)]
+        public void OnScp079LevelUpTier(PlayerAPI player, int int079)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) повысил свой tier до {int079}");
         }
 
-    [PluginEvent(ServerEventType.PlayerOpenGenerator)]
-    public void OnPlayerOpenGenerator(PlayerAPI player , Generator gen)
-    {
-        Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) открыл генератор {gen}");
-    }
+        [PluginEvent(ServerEventType.PlayerOpenGenerator)]
+        public void OnPlayerOpenGenerator(PlayerAPI player, Generator gen)
+        {
+            Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) открыл генератор {gen}");
+        }
 
         [PluginEvent(ServerEventType.PlayerCloseGenerator)]
-        public void OnPlayerCloseGenerator(PlayerAPI player , Generator gen)
+        public void OnPlayerCloseGenerator(PlayerAPI player, Generator gen)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) закрыл генератор {gen}");
-        } 
-        
+        }
+
         [PluginEvent(ServerEventType.PlayerInteractScp330)]
         public void OnPlayerInteractScp330(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) применил SCP-330");
         }
-        
+
         [PluginEvent(ServerEventType.WarheadStart)]
         public void OnWarheadStart(PlayerAPI player)
         {
-            Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) активировал процесс детонации Альфа-Боеголовки");
+            Plugin.Sender.AddToQuene(
+                $"Игрок {player.Nickname}({player.UserId}) активировал процесс детонации Альфа-Боеголовки");
         }
-        
+
         [PluginEvent(ServerEventType.WarheadStop)]
         public void OnWarheadStop(PlayerAPI player)
         {
-            Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) остановил процесс детонации Альфа-Боеголовки");
+            Plugin.Sender.AddToQuene(
+                $"Игрок {player.Nickname}({player.UserId}) остановил процесс детонации Альфа-Боеголовки");
         }
-        
+
         [PluginEvent(ServerEventType.WarheadDetonation)]
         public void OnWarheadDetonation(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) взорвал Альфа-Боеголовку");
         }
-        
+
         [PluginEvent(ServerEventType.Scp106Stalking)]
         public void OnScp106Stalking(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) переключил stalk mode за SCP-106");
         }
-        
+
         [PluginEvent(ServerEventType.Scp079UseTesla)]
         public void OnScp079UseTesla(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) активровал Tesla-ворота за SCP-079");
         }
-        
+
         [PluginEvent(ServerEventType.Scp079LockdownRoom)]
         public void OnScp079LockdownRoom(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) заблокировал комнату за SCP-079");
         }
-        
+
         [PluginEvent(ServerEventType.Scp079BlackoutZone)]
         public void OnScp079BlackoutZone(PlayerAPI player)
         {
-            Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) отключил питание в одной из зон комплекса");
+            Plugin.Sender.AddToQuene(
+                $"Игрок {player.Nickname}({player.UserId}) отключил питание в одной из зон комплекса");
         }
-        
+
         [PluginEvent(ServerEventType.Scp049ResurrectBody)]
         public void OnScp049ResurrectBody(PlayerAPI player)
         {
             Plugin.Sender.AddToQuene($"Игрок {player.Nickname}({player.UserId}) воскресил тело за SCP-049");
         }
-        
     }
+}
