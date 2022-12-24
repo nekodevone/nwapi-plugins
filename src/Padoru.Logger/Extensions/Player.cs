fb319dcc97624268b4e22f15af96d1ec0aa95bf9
JasKill <jaskll000@gmail.com>
2022-12-24 20:20:52 +0700

    chore(logger): экранируем md

--- src/Padoru.Logger/Extensions/Player.cs
+++ src/Padoru.Logger/Extensions/Player.cs
@@ -1,10 +1,15 @@
-﻿using PlayerRoles;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using PlayerRoles;
 using PlayerAPI = PluginAPI.Core.Player;
 
 namespace Padoru.Logger.Extensions
 {
     public static class Player
     {
+        private static readonly Dictionary<string, string> EscapeCache = new(1000);
+        private static readonly Regex EscapePattern = new("(\\\\|-|_|\\*|~|`|@|\\|)", RegexOptions.Compiled);
+        
         /// <summary>
         /// Получить подробную информацию о игроке
         /// </summary>
@@ -12,7 +17,7 @@ namespace Padoru.Logger.Extensions
         /// <returns></returns>
         public static string GetInfo(this PlayerAPI player)
         {
-            return $"**{player.PlayerId} - {player.Nickname} ({player.UserId})[{player.Role}]**";
+            return $"**{player.PlayerId} - {player.Nickname.Escape()} ({player.UserId})[{player.Role}]**";
         }
 
         /// <summary>
@@ -23,6 +28,22 @@ namespace Padoru.Logger.Extensions
         public static Team GetTeam(this PlayerAPI player)
         {
             return player.ReferenceHub.roleManager.CurrentRole.Team;
+        }
+        
+        /// <summary>
+        /// Экранирует Markdown символы
+        /// </summary>
+        /// <param name="input">Текст</param>
+        /// <returns>Экранированный текст</returns>
+        public static string Escape(this string input)
+        {
+            if (!EscapeCache.TryGetValue(input, out var output))
+            {
+                output = EscapePattern.Replace(input, match => $"\\{match.Value}");
+                EscapeCache[input] = output;
+            }
+
+            return output;
         }
     }
 }
